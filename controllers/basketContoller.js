bc53e6c4ef00622b4f3c48fe4b4da0d2531c32cc
Roman <kurpelroma@gmail.com>
2023-04-20 14:39:12 +0300

    Доробив корзину

--- controllers/basketContoller.js
+++ controllers/basketContoller.js
@@ -1,39 +1,108 @@
+const ApiError = require('../error/ApiError')
 const { Hookah, BasketHookah, Basket } = require('../models/models')
 const jwt = require("jsonwebtoken")
+const incrementBasketHookah = async (existingBasketHookah) => {
+    existingBasketHookah.count += 1;
+    return await existingBasketHookah.save();
+}
+
+const getBasket = async (req) =>{
+    const token = await req.headers.authorization.split(' ')[1]
+    const decode = jwt.verify(token, process.env.DB_KEY)
+    const basket = await Basket.findOne({ where: { userId: decode.id } })
+    return basket
+}
+
+const decrementBasketHookah = async (existingBasketHookah, res) => {
+    if (existingBasketHookah.count > 1) {
+        existingBasketHookah.count -= 1;
+    } else {
+        await existingBasketHookah.destroy()
+        return res.json({message: "Товар видалений з кошика"})
+    }
+    return await existingBasketHookah.save();
+}
 class basketController {
-    async create(req, res) {
-        const { id } = req.params
-        const token = await req.headers.authorization.split(' ')[1]
-        const decode = jwt.verify(token, process.env.DB_KEY)
-        const basket = await Basket.findOne({ where: { userId: decode.id } })
-        const hookah = await Hookah.findOne({ where: { id }, })
-        let basketHookah
-        const existingBasketHookah = await BasketHookah.findOne({ where: { basketId: basket.id, hookahId: hookah.id } });
-        if (existingBasketHookah) {
-            // Якщо товар уже є в кошику, збільшуємо кількість
-            existingBasketHookah.count += 1;
-            basketHookah = await existingBasketHookah.save();
-        } else {
-            // Якщо товару ще немає в кошику, створюємо новий запис
-            basketHookah = await BasketHookah.create({ basketId: basket.id, hookahId: hookah.id })
+    async create(req, res, next) {
+        try {
+            const { id } = req.params
+            const basket = await getBasket(req)
+            const hookah = await Hookah.findOne({ where: { id }, })
+            let basketHookah
+            const existingBasketHookah = await BasketHookah.findOne({ where: { basketId: basket.id, hookahId: hookah.id } });
+            if (existingBasketHookah) {
+                basketHookah = await incrementBasketHookah(existingBasketHookah)
+            } else {
+                basketHookah = await BasketHookah.create({ basketId: basket.id, hookahId: hookah.id })
+            }
+            return res.json(basketHookah)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
         }
-        return res.json(basketHookah)
     }
 
     async getAll(req, res) {
-        const token = await req.headers.authorization.split(' ')[1]
-        const decode = jwt.verify(token, process.env.DB_KEY)
-        const basket = await Basket.findOne({ where: { userId: decode.id } })
+        const basket = await getBasket(req)
         const basketHookahs = await BasketHookah.findAll({
-             where: { basketId: basket.id },
-             include: {
+            where: { basketId: basket.id },
+            include: {
                 model: Hookah,
                 attributes: ['id', 'name', 'price', 'img']
             }
-            })
+        })
         return res.json(basketHookahs)
     }
-
+    async deleteOne(req, res) {
+        try {
+            const { id } = req.params
+            const basket = await getBasket(req)
+            const hookah = await Hookah.findOne({ where: { id }, })
+            let basketHookah
+            const existingBasketHookah = await BasketHookah.destroy({ where: { basketId: basket.id, hookahId: hookah.id } });
+            return res.json({ message: "Товар видалено" })
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
+    async deleteAll(req, res) {
+        try {
+            const basket = await getBasket(req)
+            const existingBasketHookah = await BasketHookah.destroy({ where: { basketId: basket.id } });
+            return res.json({ message: "Товари видалено" })
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
+    async incrementHookah(req, res, next) {
+        try {
+            const { id } = req.params
+            const basket = await getBasket(req)
+            const hookah = await Hookah.findOne({ where: { id }, })
+            let basketHookah
+            const existingBasketHookah = await BasketHookah.findOne({ where: { basketId: basket.id, hookahId: hookah.id } });
+            if (existingBasketHookah) {
+                basketHookah = await incrementBasketHookah(existingBasketHookah)
+            }
+            return res.json(basketHookah)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
+    async decrementHookah(req, res, next) {
+        try {
+            const { id } = req.params
+            const basket = await getBasket(req)
+            const hookah = await Hookah.findOne({ where: { id }, })
+            let basketHookah
+            const existingBasketHookah = await BasketHookah.findOne({ where: { basketId: basket.id, hookahId: hookah.id } });
+            if (existingBasketHookah) {
+                basketHookah = await decrementBasketHookah(existingBasketHookah,res)
+            } 
+            return res.json(basketHookah)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
 }
 
 module.exports = new basketController()
--- routers/basketRouter.js
+++ routers/basketRouter.js
@@ -5,5 +5,8 @@ const basketController = require('../controllers/basketContoller')
 
 router.post('/:id',basketController.create)
 router.get('/',basketController.getAll)
-//delete
+router.delete('/:id',basketController.deleteOne)
+router.delete('/',basketController.deleteAll)
+router.put('/increment/:id',basketController.incrementHookah)
+router.put('/decrement/:id',basketController.decrementHookah)
 module.exports = router
